12c29aabcfdb942affbc7e5740bc83f1589e1a5d
Paul Emerson <63166942+pemerson01109@users.noreply.github.com>
2022-05-19 11:46:46 -0400

    Upload 5/19/2022 11:46:000 EST

---
+++ start.sh
@@ -0,0 +1,7 @@
+#Turn TV on Using CEC
+echo 'on 0' | cec-client -s -d 1
+sleep 10
+echo 'as' | cec-client -s -d 1
+
+#Contact Paul Emerson For Help
+#emerson.paul@outlook.com
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#Turn TV off Using CEC
+echo 'standby 0' | cec-client -s -d 1
+
+#Contact Paul Emerson For Help
+#emerson.paul@outlook.com
